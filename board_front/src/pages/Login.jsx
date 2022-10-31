5fe7b5114d40121522d5fed23a3315b5acc05fab
eunbin55 <eunbin620@naver.com>
2022-11-01 00:04:52 +0900

    로그인 input아이디 체크

--- board_front/src/pages/Login.jsx
+++ board_front/src/pages/Login.jsx
@@ -5,29 +5,29 @@ import { USER_ALL } from "../graphql/user.gql";
 const Login = () => {
     const {data} = useQuery(USER_ALL);
     console.log(data);
-    const [id, setId] = useState('');
-    const [value, setValue] = useState('')
-    function idCheck () {
-        // const mId = data.userAll[0].map((item:any) => (item.mId))
-        const userId = data.userAll[0].map((item) => (item.userId))
-        if(`${setId}` === userId ){
+    const [loginId, setLoginId] = useState('');
+    function onSubmit (e) {
+        const userId = data.userAll.map((item, index) => (item.userId))
+        console.log(userId[0]);
+        console.log(loginId);
+        if(`${loginId}` === userId[0] ){
             alert(userId);
         } else {
-            alert("로그인 실패");
+            alert('userId>>>>>>'+userId);
         }
         
     }
-
+    
     return (
-        <div>
+        <form onSubmit={onSubmit}>
             <div>ID</div>
-            <input type="id" placeholder="아이디를 입력하세요" />
+            <input type="id" placeholder="아이디를 입력하세요" value={loginId} onChange={(e) => setLoginId(e.target.value)} />
             <div>Password</div>
             <input type="password" placeholder="비밀번호를 입력하세요" />
             <button variant="primary" type="submit" >
                 LOGIN
             </button>
-        </div>
+        </form>
     )
 }
 
